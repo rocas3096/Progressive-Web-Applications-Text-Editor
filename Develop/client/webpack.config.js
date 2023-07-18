91b2ac7f7fb38dccc994b40ce127475bcb824b37
rocas3096 <ozavala3096@gmail.com>
2023-07-17 21:38:10 -0500

    Add ./ to webpack line 40

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -37,7 +37,7 @@ module.exports = () => {
         theme_color: "#000000",
         icons: [
           {
-            src: path.resolve("src/images/logo.png"),
+            src: path.resolve("./src/images/logo.png"),
             sizes: [96, 128, 192, 256, 384, 512],
             destination: path.join("assets", "icons"),
           },
